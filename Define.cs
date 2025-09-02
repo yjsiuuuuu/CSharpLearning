ff1dd0245b6227fbcbc7f27e537c961f956c43c5
yjs <yjs8480@naver.com>
2025-09-02 12:08:31 +0900

    첫 커밋 - practice 브랜치, 코드 파일과 .gitignore만 포함

---
+++ Define.cs
@@ -0,0 +1,6 @@
+﻿public enum ITEM_GRADE
+{
+    UNIQUE = 10000,
+    RARE = 11000,
+    NORMAL = 12000,
+}
---
+++ GiftBox.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp3;
+using GiftBoxNamespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp3.Program;
+
+namespace GiftBoxNamespace
+{
+    class GiftBox
+    {
+
+        public string Letter;
+        public int Money;
+
+        public ITEM_GRADE Grade; //유니크 레어 노말
+
+
+    }
+
+}
